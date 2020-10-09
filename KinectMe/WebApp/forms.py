bac80a6bf3f31c4d8924d30a4c78c59fb45dbb3c
jeremyMarz <70720613+jeremymarzAtos@users.noreply.github.com>
2020-10-09 12:00:05 -0500

    login and user creation functionality

--- KinectMe/WebApp/forms.py
+++ KinectMe/WebApp/forms.py
@@ -2,3 +2,10 @@ from django import forms
 from .models import *
 
 
+class AddForm(forms.Form):
+        user_name = forms.CharField(label="Username", widget=forms.TextInput(), required = True)
+        password = forms.CharField(label="Password", widget=forms.PasswordInput(), required = True)
+        first_name = forms.CharField(label="First Name", widget=forms.TextInput(), required = True)
+        last_name = forms.CharField(label="Last Name", widget=forms.TextInput(), required = True)
+        email = forms.CharField(label="Email Address", widget=forms.TextInput(), required = True)
+        age = forms.CharField(label="Age", widget=forms.TextInput(), required = True)
--- KinectMe/WebApp/views.py
+++ KinectMe/WebApp/views.py
@@ -1,18 +1,54 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
-from .models import Profile
+from .models import Profile, Event
 from django.shortcuts import render, get_object_or_404
 from django.template.defaultfilters import slugify
+from django.contrib.auth.decorators import login_required
+from WebApp.forms import *
+from django.urls import reverse
+
 
 def homepage(request):
-    return render(request, 'site/homepage.html')
+    context ={
+        'Profile':Profile,
+    }
+    return render(request, 'site/homepage.html', context=context)
 
 def signup(request):
     return render(request, 'site/signup.html')
 
 def login(request):
-    return render(request, 'site/login.html')
+    return render(request, 'registration/login.html')
+
+@login_required(login_url='/accounts/login/')
+def Add(request):
+    form = AddForm(request.POST)
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['user_name']
+            User.objects.create(username=user_name)
+            user = User.objects.get(username=user_name)
+            user.password = form.cleaned_data['password']
+            user.save()
+            Profile.objects.create(user_name=user_name)
+            profile = Profile.objects.get(user_name=user.username)
+            profile.user = user
+            profile.first_name = form.cleaned_data['first_name']
+            profile.last_name = form.cleaned_data['last_name']
+            profile.email = form.cleaned_data['email']
+            profile.age = form.cleaned_data['age']
+
+            profile.save()
+            
+        return HttpResponseRedirect(reverse( 'home:homepage') )
+    else:
+        form = AddForm(request.POST)
+        context = {
+            'form': form,
+        }
+    return render(request, "site/signup.html", context)
 
 class Dash(generic.ListView):
     model = Profile
