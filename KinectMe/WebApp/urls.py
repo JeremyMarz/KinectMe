04a3764ffb8ceb2752905862f1b46c146cc978f5
jeremyMarz <70720613+jeremymarzAtos@users.noreply.github.com>
2020-10-09 14:28:10 -0500

    user edit page

--- KinectMe/WebApp/urls.py
+++ KinectMe/WebApp/urls.py
@@ -7,4 +7,6 @@ app_name = 'home'
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('signup/', views.Add, name='signup'),
+    path('update/', views.Update, name='update'),
+
 ]
